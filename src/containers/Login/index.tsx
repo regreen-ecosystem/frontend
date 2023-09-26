52c611ea1b1bb8ac3ff3bc774924d76905f2a02e
sars2811 <sarthak_g@me.iitr.ac.in>
2023-09-26 14:30:10 +0530

    feat: Add Login PAge.

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -1,24 +1,82 @@
-import { Button, Checkbox, Link, TextField, Typography } from '@mui/material';
-import { grey } from '@mui/material/colors';
+import { Button, Link, TextField, Typography } from '@mui/material';
 import React from 'react';
 import { makeStyles } from 'tss-react/mui';
-import Heading from './Heading';
+import { ReactComponent as Logo } from '../../assets/images/logo.svg';
 
 const useStyles = makeStyles()((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
-    padding: theme.spacing(4),
+    padding: '0 2rem',
     alignContent: 'center',
-    flexWrap: 'wrap',
+    justifyContent: 'center',
+    width: '50vw',
     height: '100vh',
+    backgroundColor: theme.palette.common.white,
+    flexWrap: 'wrap',
   },
-
-  box: {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignContent: 'center',
+    padding: '3rem 0px',
+    backgroundColor: theme.palette.primary.light,
+    borderRadius: '20px',
+    boxShadow: '0px 20px 80px 0px rgba(35, 35, 35, 0.15)',
+    width: '60%',
+    flexWrap: 'wrap',
+  },
+  heading: {
+    color: theme.palette.text.primary,
+    fontSize: theme.typography.body1.fontSize,
+    fontWeight: '300',
+  },
+  title: {
+    color: theme.palette.primary.main,
+    fontSize: theme.typography.h5.fontSize,
+    fontWeight: '500',
+    marginBottom: '1rem',
+  },
+  button: {
+    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.common.white,
+    fontSize: theme.typography.body1.fontSize,
+    fontWeight: '400',
+    borderRadius: '20px',
+    padding: '0.5rem 0',
+    width: '80%',
+    margin: '1rem 0 1rem 0 ',
+  },
+  logo: {
+    position: 'absolute',
+    top: '20px',
+    left: '20px',
+  },
+  titleContainer: {
     display: 'flex',
     flexDirection: 'row',
+    justifyContent: 'center',
     alignItems: 'center',
-    alignContent: 'center',
+    marginBottom: '2rem',
+    // wordWrap: 'no-wrap',
+  },
+  decoratedTitle: {
+    color: theme.palette.secondary.main,
+    fontWeight: '500',
+    display: 'inline',
+  },
+  link: {
+    color: theme.palette.secondary.main,
+    marginLeft: '0.2rem',
+    textDecoration: 'none',
+  },
+  forgotLink: {
+    color: theme.palette.text.primary,
+    textAlign: 'right',
+    alignSelf: 'flex-end',
+    marginTop: '0.5rem',
+    // textDecoration: 'none',
   },
 }));
 
@@ -27,44 +85,72 @@ const Login: React.FC = () => {
 
   return (
     <div className={classes.root}>
-      <Heading />
-      <Typography variant='h6' style={{ fontWeight: 400 }}>
-        Email
-      </Typography>
-      <TextField
-        id='outlined-basic'
-        variant='outlined'
-        size='small'
-        style={{ marginBottom: '0.5rem' }}
-      />
-      <Typography variant='h6' style={{ fontWeight: 400 }}>
-        Password
-      </Typography>
-      <TextField id='outlined-basic' variant='outlined' size='small' />
-      <div
-        className={classes.box}
-        style={{ justifyContent: 'space-between', marginBottom: '1rem' }}
-      >
-        <div className={classes.box}>
-          <Checkbox size='small' />
-          <Typography variant='body1' style={{ fontWeight: 200 }}>
-            Remember me
+      <Logo className={classes.logo} />
+      <div className={classes.titleContainer}>
+        <Typography variant='h5'>
+          {'Welcome Back to '}
+          <Typography variant='h5' className={classes.decoratedTitle}>
+            {'Regreen Ecosystem'}
           </Typography>
-        </div>
-        <Link href='/forgot-password'>forgot password?</Link>
+        </Typography>
       </div>
-      <Button variant='contained' style={{ marginBottom: '0.8rem' }}>
-        Sign In
-      </Button>
-      <Button variant='outlined' style={{ marginBottom: '0.8rem' }}>
-        Sign In with Google
-      </Button>
-      <Typography
-        variant='body1'
-        style={{ fontWeight: 200, textAlign: 'center' }}
-      >
-        Don&#39;t have an account? <Link href='/signup'>Sign Up</Link>
-      </Typography>
+      <div className={classes.container}>
+        <Typography className={classes.title}>Log In</Typography>
+        <Typography className={classes.heading}>Email</Typography>
+        <TextField
+          id='outlined-basic'
+          variant='outlined'
+          size='small'
+          style={{ marginBottom: '0.5rem', width: '80%' }}
+          placeholder='Email'
+        />
+        <Typography className={classes.heading}>Password</Typography>
+        <TextField
+          id='outlined-basic'
+          variant='outlined'
+          size='small'
+          placeholder='Password'
+          type='password'
+          style={{ width: '80%' }}
+        />
+        <Link href='/forgot-password' className={classes.forgotLink}>
+          Forgot Password?
+        </Link>
+        <Button className={classes.button}>{'Log In'}</Button>
+        <Typography
+          variant='body1'
+          style={{ fontWeight: 200, textAlign: 'center' }}
+        >
+          Don&#39;t have an account?{' '}
+          <Link href='/signup' className={classes.link}>
+            Sign Up
+          </Link>
+        </Typography>
+        {/* <div
+          className={classes.box}
+          style={{ justifyContent: 'space-between', marginBottom: '1rem' }}
+        >
+          <div className={classes.box}>
+            <Checkbox size='small' />
+            <Typography variant='body1' style={{ fontWeight: 200 }}>
+              Remember me
+            </Typography>
+          </div>
+          <Link href='/forgot-password'>forgot password?</Link>
+        </div> */}
+        {/* <Button variant='contained' style={{ marginBottom: '0.8rem' }}>
+          Sign In
+        </Button>
+        <Button variant='outlined' style={{ marginBottom: '0.8rem' }}>
+          Sign In with Google
+        </Button>
+        <Typography
+          variant='body1'
+          style={{ fontWeight: 200, textAlign: 'center' }}
+        >
+          Don&#39;t have an account? <Link href='/signup'>Sign Up</Link>
+        </Typography> */}
+      </div>
     </div>
   );
 };
